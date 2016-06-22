c54002080c1989914235a00881bf6340e8e20b3d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-06-22 18:22:23 +0200

    enabled travis ... updated setup.py ... refactored tests

--- setup.py
+++ setup.py
@@ -5,26 +5,33 @@ This module contains the tool of birdhousebuilder.recipe.pywps
 from setuptools import find_packages
 from setuptools import setup
 
+name = 'birdhousebuilder.recipe.pywps'
+
 version = '0.5.0'
-description = 'A Buildout recipe to install and configure PyWPS Web Processing Service with Anaconda.'
+description = 'A Buildout recipe to install and configure PyWPS Web Processing Service with conda.'
 long_description = (
     open('README.rst').read() + '\n' +
     open('AUTHORS.rst').read() + '\n' +
     open('CHANGES.rst').read()
 )
 
-entry_point = 'birdhousebuilder.recipe.pywps'
-entry_points = {"zc.buildout": [
-                            "default = %s:Recipe" % entry_point,
-                          ],
-                "zc.buildout.uninstall": [
-                            "default = %s:uninstall" % entry_point,
-                          ],
-                       }
+entry_points = '''
+[zc.buildout]
+default = %(name)s:Recipe
+[zc.buildout.uninstall]
+default = %(name)s:uninstall
+''' % globals()
 
-tests_require = ['zope.testing', 'zc.buildout', 'manuel']
+reqs = ['setuptools',
+        'zc.buildout',
+        'zc.recipe.deployment',
+        'Mako',
+        'birdhousebuilder.recipe.conda',
+        'birdhousebuilder.recipe.supervisor',
+        'birdhousebuilder.recipe.nginx']
+tests_reqs = ['zc.buildout', 'zope.testing']
 
-setup(name='birdhousebuilder.recipe.pywps',
+setup(name=name,
       version=version,
       description=description,
       long_description=long_description,
@@ -36,25 +43,16 @@ setup(name='birdhousebuilder.recipe.pywps',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'License :: OSI Approved :: BSD License',
       ],
-      keywords='buildout recipe pywps wps birdhouse conda anaconda',
+      keywords='buildout recipe pywps wps birdhouse conda',
       author='Birdhouse',
+      author_email="wps-dev at dkrz.de",
       url='https://github.com/bird-house/birdhousebuilder.recipe.pywps',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['birdhousebuilder', 'birdhousebuilder.recipe'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['setuptools',
-                        'zc.buildout',
-                        # -*- Extra requirements: -*-
-                        'Mako',
-                        'birdhousebuilder.recipe.conda',
-                        'birdhousebuilder.recipe.supervisor',
-                        'birdhousebuilder.recipe.nginx',
-                        'zc.recipe.deployment',
-                        ],
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite='birdhousebuilder.recipe.pywps.tests.test_docs.test_suite',
+      license='Apache License 2',
+      install_requires = reqs,
+      extras_require=dict(tests=tests_reqs),
       entry_points=entry_points,
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages = ['birdhousebuilder', 'birdhousebuilder.recipe'],
+      include_package_data = True,
+      zip_safe = False,
       )
