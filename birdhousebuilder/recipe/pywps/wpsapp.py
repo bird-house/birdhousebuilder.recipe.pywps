a1407ef32dcb3b1f57fd40e9938deaab9bdce81b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-31 19:42:56 +0200

    simplified app template

--- birdhousebuilder/recipe/pywps/wpsapp.py
+++ birdhousebuilder/recipe/pywps/wpsapp.py
@@ -1,20 +1,6 @@
 import os
 from pywps.app.Service import Service
-from pywps import Process
-import logging
-logger = logging.getLogger('PYWPS')
 
-# processes need to be installed in PYTHON_PATH
-# from processes.sleep import Sleep
-
-import sys
-import inspect
-import emu.processes
-processes = []
-for name, clazz in inspect.getmembers(sys.modules['emu.processes'],
-                                      inspect.isclass):
-    if issubclass(clazz, Process):
-        processes.append(clazz())
-        logger.info("init process %s", name)
+from emu.processes import processes
 
 application = Service(processes, [os.environ['PYWPS_CFG']])
