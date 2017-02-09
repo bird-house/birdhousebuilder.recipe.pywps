77fa4cf9c0ae5f8c38e16bb6f948d3674d8c1d61
Pingu Carsti <pingu@janis.local>
2017-02-09 11:14:49 +0000

    added extra_options test case

--- birdhousebuilder/recipe/pywps/tests/test_unit.py
+++ birdhousebuilder/recipe/pywps/tests/test_unit.py
@@ -6,6 +6,8 @@ class UtilsTestCase(unittest.TestCase):
 
     def test_extra_options(self):
         from birdhousebuilder.recipe.pywps import parse_extra_options
-        parse_extra_options()
+        extra_options = parse_extra_options("esmval_root=/path/to/esmval archive_root=/path/to/archive")
+        assert extra_options['esmval_root'] == "/path/to/esmval"
+        assert extra_options['archive_root'] == "/path/to/archive"
 
 
