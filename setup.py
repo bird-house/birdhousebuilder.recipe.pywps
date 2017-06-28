03a9d614fe2a46fa6fb11b9e0d6d766ae7514540
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-28 17:28:59 +0200

    version 0.9.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 name = 'birdhousebuilder.recipe.pywps'
 
-version = '0.9.0'
+version = '0.9.1'
 description = 'A Buildout recipe to install and configure PyWPS Web Processing Service with conda.'
 long_description = (
     open('README.rst').read() + '\n' +
